93262a4aa32169fa383b802b5092c58d4d34e02e
Alexander Roth <roth.alex@outlook.com>
2023-12-05 12:12:04 +0100

    added circDiagram to displayplayer

--- src/client/DisplayPlayer.tsx
+++ src/client/DisplayPlayer.tsx
@@ -5,6 +5,7 @@ import PlayerNameWithBackground from "./PlayerNameWithBackground";
 import {getPlayTime} from "./helperFunctions"
 import HeroInfoCard from "./HeroInfoCard";
 import SankeyDiagram from "./SankeyDiagram";
+import { CircDiagramPicker } from "./CircularPacking/CircDiagramPicker";
 
 
 const DisplayPlayer = () => {
@@ -26,6 +27,7 @@ const DisplayPlayer = () => {
                 } 
                 <PlayerAvatar imageUrl={playerData.summary.avatar}/>
                 <SankeyDiagram playerData={playerData}/>
+                <CircDiagramPicker width={500} height={500} data={playerData}/>
                 {console.log(getPlayTime(playerData, true))}
                 <HeroInfoCard HeroData={playerData.stats?.pc?.competitive?.heroes_comparisons} Herostats={playerData.stats?.pc?.competitive?.career_stats}/>
             </>
