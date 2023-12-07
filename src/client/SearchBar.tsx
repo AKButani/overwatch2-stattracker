c6235bc7de8948b7783749c734a4306535306df1
Arjun Butani <abutani@ethz.ch>
2023-12-07 11:01:27 +0100

    Improved Search Bar and changed page title

--- src/client/SearchBar.tsx
+++ src/client/SearchBar.tsx
@@ -1,5 +1,6 @@
 // SearchBar.js
-
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const SearchBar = (props: {searchTerm: string, setSearchTerm: React.Dispatch<React.SetStateAction<string>>, onSearch: () => void}) => {
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -8,15 +9,15 @@ const SearchBar = (props: {searchTerm: string, setSearchTerm: React.Dispatch<Rea
   };
 
   return (
-    <div>
+    <div className="search-bar">
       <input
         type="text"
         placeholder="Enter Player name (eg. emongg-11183)"
         value={props.searchTerm}
         onChange={handleInputChange}
-        style={{ width: '500px', textAlign: 'center'}}
+        style={{ marginBottom: 0, width: '500px', border: 'none'}}
       />
-      <button onClick={props.onSearch}>Search</button>
+      <FontAwesomeIcon onClick={props.onSearch} icon={faSearch} size='lg' />
     </div>
   );
 };
