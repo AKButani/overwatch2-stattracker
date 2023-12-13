c06f4072558c920c61611c18130191625af0d4ef
Alexander Roth <roth.alex@outlook.com>
2023-12-13 20:01:16 +0100

    better value dispaly

--- src/client/CircularPacking/CircDiagramPicker.tsx
+++ src/client/CircularPacking/CircDiagramPicker.tsx
@@ -3,21 +3,51 @@ import { CircDiagram } from "./CircDiagram";
 import { HEROES_KEYS, PlayerCareer, comparisonCategory, mode, platform} from "../types";
 import { getHeroComparison} from "../helperFunctions";
 import { SelectedModeContext } from "../DisplayPlayer";
+import { valueFunction, getDisplayValueFunction} from "./circTypes";
 
 export const CircDiagramPicker = (props: { key: number; data: PlayerCareer; width: number; height: number }) => {
-  type vF = ((data:PlayerCareer, hero:HEROES_KEYS) => number);
-  const [valueFunction, setValueFunction] = useState<vF>(() => ((data:PlayerCareer, hero:HEROES_KEYS) => 5));
+  const [valueFunction, setValueFunction] = useState<valueFunction>(() => ((data:PlayerCareer, hero:HEROES_KEYS) => 5));
+  const [displayValueFunction, setDisplayValueFunction] = useState<getDisplayValueFunction>(() => ((value:number) => "-"));
   const selectedMode = useContext(SelectedModeContext);
-
+  
     const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
       if(event.target.value == "none"){
           setValueFunction (() => ((data:PlayerCareer, hero:HEROES_KEYS) => 5));
       } else {
           setValueFunction(() => ((data:PlayerCareer, hero:HEROES_KEYS):number => getHeroComparison(data, hero, "both", selectedMode , event.target.value as comparisonCategory)));
       }
+
+      switch(event.target.value) {
+        case "none":
+          setDisplayValueFunction(() => ((value:number) => "-"));
+          break;
+        case "time_played":
+          setDisplayValueFunction(() => ((value:number) => (value/3600).toFixed(1) + " h"));
+          break;
+        case "games_won":
+          setDisplayValueFunction(() => ((value:number) => value + ""));
+          break;
+        case "weapon_accuracy":
+          setDisplayValueFunction(() => ((value:number) => value + "%"));
+          break;
+        case "win_percentage":
+          setDisplayValueFunction(() => ((value:number) => value + "%"));
+          break;
+        case "eliminations_per_life":
+          setDisplayValueFunction(() => ((value:number) => value + ""));
+          break;
+        case "critical_hit_accuracy":
+          setDisplayValueFunction(() => ((value:number) => value + "%"));
+          break;
+        case "multikill_best":
+          setDisplayValueFunction(() => ((value:number) => value + ""));
+          break;
+        case "objective_kills":
+          setDisplayValueFunction(() => ((value:number) => value + ""));
+          break;
+      }
        
     }
-  
 
   return (
     <div style={{ textAlign: 'center' }}>
@@ -34,7 +64,7 @@ export const CircDiagramPicker = (props: { key: number; data: PlayerCareer; widt
         <option value="objective_kills">Objective Kills</option>
         {/* Add more options as needed */}
       </select>
-      <CircDiagram width={props.width} height={props.height} data={props.data} valueFunction={valueFunction} />
+      <CircDiagram width={props.width} height={props.height} data={props.data} valueFunction={valueFunction} getDisplayValue={displayValueFunction}/>
     </div>
   );
 };
