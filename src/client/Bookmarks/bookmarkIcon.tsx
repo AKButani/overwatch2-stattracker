53db94090d98f96b3e68667b4cc5682fdf565572
Arjun Butani <abutani@student.ethz.ch>
2023-12-17 18:49:34 +0100

    bookmark icon works correctly when changing players

--- src/client/Bookmarks/bookmarkIcon.tsx
+++ src/client/Bookmarks/bookmarkIcon.tsx
@@ -5,13 +5,26 @@ import { faBookmark} from '@fortawesome/free-solid-svg-icons';
 import { HEROES_KEYS, PlayerCareer, PlayerSummary, platform} from "../types";
 
 
+
+
 export const BookmarkIcon = (props: {summary: PlayerSummary, username: string}) => {
     
     const storedBookmarked = localStorage.getItem("bookmarkedPlayers");
     var bookmarks : Array<[string , string | undefined, string | undefined]> = [];
+    //the type is username, iconUrl, and namecardurl
     if(storedBookmarked != null){
         bookmarks = JSON.parse(storedBookmarked);
     }
+
+    const checkifBookmarked = (playerName: string) => {
+        for (let player of bookmarks) {
+            if(player[0] == playerName){
+                return true;
+            }
+        }
+        return false;
+    }
+    
     const [isBookmarked, setIsBookmarked] = useState(bookmarks.filter((item) => item[0] == props.username).length > 0);
     const handleClick = () => {
         const newBookmarkState = !isBookmarked;
@@ -31,7 +44,7 @@ export const BookmarkIcon = (props: {summary: PlayerSummary, username: string}) 
         console.log("Bookmarks", bookmarks);
     }
     return (
-        <div style={{ cursor: 'pointer', color: isBookmarked ? 'grey' : 'black' }}>
+        <div style={{ cursor: 'pointer', color: checkifBookmarked(props.username) ? 'grey' : 'black' }}>
           <FontAwesomeIcon icon={faBookmark} onClick={() => (handleClick())} size="3x"/>
         </div>
     );
