0c02ddbc54b1333c61db9e48938ef6521ec18945
Elia Haemmerli <eliah@student.ethz.ch>
2023-12-18 22:21:39 +0100

    tiny tweaks

--- src/client/DisplayPlayer.tsx
+++ src/client/DisplayPlayer.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { PlayerDataContext } from "./App";
 import HeroInfoCard from "./HeroInfoCard";
 import SankeyDiagram from "./SankeyDiagram";
